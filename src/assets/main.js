5b4aa82d1852becd240211d9be367e79d2d3f693
AndrewArian <abradan97@gmail.com>
2023-03-24 02:38:19 -0300

    fixed bugs

--- src/assets/main.js
+++ src/assets/main.js
@@ -21,7 +21,7 @@ async function fetchData(urlAPI){
         const videos = await fetchData(APIYT)
         let view = `
         ${videos.items.map(video =>`
-        <a href="https://www.youtube.com/watch?v=${video.snippet.resourceId.videoId}" target="_blank">
+        <a href="https://www.youtube.com/watch?v=${video.id.videoId}" target="_blank">
             <div class="group relative">
             
                 <div class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
